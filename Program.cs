19149faf70c44b656410e6b0f337620f361a074d
Gyurcsaklj <gyurcsakl@kkszki.hu>
2025-09-10 09:29:19 +0200

    idk

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idk
+{
+    public class Szemely
+    {
+        string nev;
+        int kor;
+
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+        /*
+        public Szemely(string Nev, int Kor)
+        {
+            nev = Nev;
+            kor = Kor;
+        }
+
+        public string Kiir()
+        {
+            return $"A tanuló neve: {nev} és {kor} éves.";
+        }
+        */
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Szemely tanulo1 = new Szemely();
+            tanulo1.Nev = "Gábor";
+            Console.WriteLine(tanulo1.Nev);
+            /*
+            Szemely tanulo1 = new Szemely("Peti", 34);
+            Console.WriteLine(tanulo1.Kiir());
+
+            Szemely tanulo2 = new Szemely("Jani", 38);
+            Console.WriteLine(tanulo2.Kiir());
+            */
+        }
+    }
+}
