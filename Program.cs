db0a5994fe7e23cbd040a8d8d5d25b4aa6392f5c
Gyurcsaklj <gyurcsakl@kkszki.hu>
2025-09-17 08:16:28 +0200

    hallgato added

--- Program.cs
+++ Program.cs
@@ -17,6 +17,12 @@ namespace idk
             set { nev = value; }
         }
 
+        public int Kor
+        {
+            get { return kor; }
+            set { kor = value; }
+        }
+
         /*
         public Szemely(string Nev, int Kor)
         {
@@ -31,14 +37,52 @@ namespace idk
         */
     }
 
+    public class Bankszamla
+    {
+        private int egyenleg;
+
+        public int Egyenleg
+                {
+            get { return egyenleg; }
+            set 
+            { 
+                if (value >= 0)
+                    egyenleg = value;
+                else
+                    Console.WriteLine("Az egyenleg nem lehet negatív!");
+            }
+        }
+
+        public void betesz()
+        {
+            
+        }
+
+        public void kivesz()
+        {
+
+        }
+    }
+
+    public class Hallgato : Szemely
+    {
+        public string neptunKod { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Szemely tanulo1 = new Szemely();
             tanulo1.Nev = "Gábor";
-            tanulo1.kor="25";
+            tanulo1.Kor=25;
+
             Console.WriteLine(tanulo1.Nev);
+            Console.WriteLine(tanulo1.Kor);
+
+            Bankszamla szamla1 = new Bankszamla();
+            szamla1.Egyenleg = 1000;
+            Console.WriteLine(szamla1.Egyenleg);
             /*
             Szemely tanulo1 = new Szemely("Peti", 34);
             Console.WriteLine(tanulo1.Kiir());
